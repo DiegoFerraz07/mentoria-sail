1d3494721f50ce91d0707a49a64d44735cc0c4fe
DiegoFerraz07 <dihferraz-01@hotmail.com>
2024-03-30 10:21:43 -0300

    model created

---
+++ app/Models/Orders.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model
+{
+    use HasFactory;
+
+    protected $table = 'orders';
+    protected $fillable = [
+        'numero_order',
+        'description_order',
+        'tax_order',
+        'icms_order',
+        'total_value_order',
+        'obs_order',
+        'name_supply',
+        'cnpj_supply',
+        'name_client',
+        'cpf_client',
+        'cnpj_client',
+        'address_client',
+        'orders_itens'=> 'json',
+    ];
+
+    public function fillOrders($request) : Orders
+    {
+        $this->numero_order = $request->numeroOrder;
+        $this->description_order = $request->descriptionOrder;
+        $this->tax_order = $request->taxOrder;
+        $this->total_value_order = $request->totalValueOrder;
+        $this->obs_order = $request->obsOrder;
+        $this->name_supply = $request->nameSupply;
+        $this->cnpj_supply = $request->cnpjSupply;
+        $this->name_client = $request->nameClient;
+        $this->cpf_client = $request->cpfClient;
+        $this->cnpj_client = $request->cnpjClient;
+        $this->address_client = $request->addressClient;
+        $this->orders_itens = $request->ordersItens;
+        return $this;
+    }
+}
